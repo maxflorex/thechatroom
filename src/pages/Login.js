cf8c141901370f85bf1d5f119f3506048b2f81a9
Max Flores <max@maxflores.dev>
2022-05-01 09:51:51 -0600

    Cloud Setup

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+
+	const btntw = 'mt-2 bg-slate-300 py-4 px-6 rounded-lg hover:bg-slate-900 hover:text-white duration-500 cursor-pointer mr-auto active:scale-110'
+	const inputtw = 'bg-slate-100 p-4 rounded-lg w-96 focus:bg-slate-300 outline-none duration-500 active:scale-110'
+
+	return (
+		<div className="container mx-auto">
+			<div className="grid grid-cols-2 h-[80vh]">
+				<img src="https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="Login" className='w-full h-full object-cover' />
+				<div className="flex flex-col p-8 justify-center items-center">
+					<form className='flex flex-col gap-4'>
+						<p className='italic'>Email:</p>
+						<input className={inputtw} type="email" placeholder='Enter Email...' />
+						<p className='text-xs text-slate-300'>Your information will never be shared</p>
+						<p className='italic'>Password:</p>
+						<input className={inputtw} type="password" placeholder='Enter Password...' />
+						<span className={btntw}>Login</span>
+					</form>
+					<div className="pt-8">
+						<h1>Don't have an account? <Link to='/signup' className='text-amber-500 hover:text-amber-300 duration-300'>Signup</Link></h1>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	)
 }
 
 export default Login
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,9 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { HiOutlineCloudUpload } from 'react-icons/hi'
 
 const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
+
+	const btntw = 'mt-2 bg-slate-300 py-4 px-6 rounded-lg hover:bg-slate-900 hover:text-white duration-500 cursor-pointer mr-auto active:scale-110'
+	const inputtw = 'bg-slate-100 p-4 rounded-lg w-96 focus:bg-slate-300 outline-none duration-500 active:scale-110'
+
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [name, setName] = useState('');
+
+	// IMAGE UPLOAD states
+
+	const [image, setImage] = useState(null);
+	const [uploading, setUploading] = useState(false);
+	const [imagePrev, setImagePrev] = useState(null);
+
+	// FUNCTION - VALIDATION
+
+	const ValidateImg = (e) => {
+		const file = e.target.files[0];
+		if (file.size >= 1048576) {
+			return alert('Max file is 1mb')
+		} else {
+			setImage(file);
+			setImagePrev(URL.createObjectURL(file))
+		}
+	}
+
+	// FUNCTION - SIGN UP
+
+	const handleSignUp = (e) => {
+		e.preventDefault();
+		if (!image) return alert('Please add your profile picture');
+		// const url = await uplloaImage(image);
+	}
+
+
+	// FUNCTION - UPLOAD IMAGE
+
+	const uploadImage = async () => {
+		const data = new FormData()
+	}
+
+	// CONSOLE
+
+	console.log(name, email, password)
+
+	// LINKS
+
+	const img = 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80'
+
+
+	return (
+		<div className="container mx-auto">
+			<div className="grid grid-cols-2 h-[80vh]">
+				<div className="flex flex-col p-8 justify-center items-start relative">
+					<h1 className='text-4xl font-bold mb-8'>Sing up</h1>
+					<label for='image-upload' className='relative'>
+						<img src={imagePrev || img} alt="Headshot" className='w-40 h-40 rounded-full object-cover object-top mb-12 cursor-pointer hover:scale-110 active:scale-90 duration-300' />
+						<HiOutlineCloudUpload  className='absolute bottom-0 right-0 bg-teal-500 p-2 rounded-full text-white text-4xl cursor-pointer mb-4 hover:bg-amber-500'/>
+					</label>
+					<input type="file" id='image-upload' hidden accept='image/png, image/jpeg' onChange={ValidateImg} />
+					<form className='flex flex-col gap-4'>
+						<p className='italic'>Name:</p>
+						<input className={inputtw} onChange={(e) => setName(e.target.value)} type="email" placeholder='Enter Your Name...' />
+						<p className='italic'>Email:</p>
+						<input className={inputtw} onChange={(e) => setEmail(e.target.value)} type="email" placeholder='Enter Email...' />
+						<p className='text-xs text-slate-300'>Your information will never be shared</p>
+						<p className='italic'>Password:</p>
+						<input className={inputtw} onChange={(e) => setPassword(e.target.value)} type="password" placeholder='Enter Password...' />
+						<span className={btntw} onClick={handleSignUp}>Signup</span>
+					</form>
+					<div className="pt-8">
+						<h1>Already have an account? <Link to='/login' className='text-amber-500 hover:text-amber-300 duration-300'>Login</Link></h1>
+					</div>
+				</div>
+				<img src="https://images.unsplash.com/photo-1580130037032-b1d3878e348b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1365&q=80" alt="Login" className='w-full h-full object-cover' />
+			</div>
+		</div>
+	)
 }
 
 export default SignUp
